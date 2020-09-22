f4ccead2c29a34f017c6b330fcb93408422af50b
sptuan <sptuan@steinslab.io>
2020-09-22 20:29:21 +0800

    [Add] CRUD in user and session

---
+++ data/user.go
@@ -0,0 +1,151 @@
+package data
+
+import "time"
+
+type User struct {
+	Id        int
+	Uuid      string
+	Name      string
+	Email     string
+	Password  string
+	CreatedAt time.Time
+}
+
+type Session struct {
+	Id        int
+	Uuid      string
+	Email     string
+	UserId    int
+	CreatedAt time.Time
+}
+
+// user: create a new session
+func (user *User) CreateSession() (session Session, err error) {
+	crud := "insert into sessions (uuid, email, user_id, created_at) values ($1, $2, $3, $4) returning id, uuid, email, user_id, created_at"
+	stmt, err := Db.Prepare(crud)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(createUUID(), user.Email, user.Id, user.CreatedAt).Scan(&session.Id, &session.Uuid, &session.Email, &session.UserId, &session.CreatedAt)
+	return
+}
+
+// user: return an existed user session
+func (user *User) Session() (session Session, err error) {
+	session = Session{}
+	err = Db.QueryRow("SELECT id, uuid, email, user_id, created_at FROM sessions WHERE user_id = $1", user.Id).
+		Scan(&session.Id, &session.Uuid, &session.Email, &session.UserId, &session.CreatedAt)
+	return
+}
+
+// user: check session in database
+func (session *Session) Check() (valid bool, err error) {
+	err = Db.QueryRow("SELECT id, uuid, email, user_id, created_at FROM sessions WHERE uuid = $1", session.Uuid).
+		Scan(&session.Id, &session.Uuid, &session.Email, &session.UserId, &session.CreatedAt)
+	if err != nil {
+		valid = false
+		return
+	}
+	if session.Id != 0 {
+		valid = true
+	}
+	return
+}
+
+// session: delete session by uuid
+func (session *Session) DeleteByUUID() (err error) {
+	crud := "delete from sessions where uuid = $1"
+	stmt, err := Db.Prepare(crud)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(session.Uuid)
+	return
+}
+
+// session: get user
+func (session *Session) User() (user User, err error) {
+	user = User{}
+	err = Db.QueryRow("SELECT id, uuid, name, email, created_at FROM users WHERE id = $1",
+		session.UserId).Scan(&user.Id, &user.Uuid, &user.Name, &user.Email, &user.CreatedAt)
+	return
+}
+
+// user: Create new user
+// describe usage with return a ID.
+// see https://github.com/sausheong/gwp
+func (user *User) Create() (err error) {
+	// Postgres does not automatically return the last insert id, because it would be wrong to assume
+	// you're always using a sequence.You need to use the RETURNING keyword in your insert to get this
+	// information from postgres.
+	crud := "insert into users (uuid, name, email, password, created_at, modified_at) values ($1, $2, $3, $4, $5, $5) returning id, uuid, created_at"
+	stmt, err := Db.Prepare(crud)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	// use QueryRow to return a row and scan the returned id into the User struct
+	err = stmt.QueryRow(createUUID(), user.Name, user.Email, Encrypt(user.Password), time.Now()).Scan(&user.Id, &user.Uuid, &user.CreatedAt)
+	return
+}
+
+// user: Delete a user
+func (user *User) Delete() (err error) {
+	crud := "delete from users where id = $1"
+	stmt, err := Db.Prepare(crud)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(user.Id)
+	return
+}
+
+// user: update infomations
+func (user *User) Update() (err error) {
+	stmt, err := Db.Prepare("update users set name=$2, email=$3 where id = $1")
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(user.Id, user.Name, user.Email)
+	return
+}
+
+// Get all users in the database and returns it
+func Users() (users []User, err error) {
+	rows, err := Db.Query("SELECT id, uuid, name, email, password, created_at FROM users")
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		user := User{}
+		if err = rows.Scan(&user.Id, &user.Uuid, &user.Name, &user.Email, &user.Password, &user.CreatedAt); err != nil {
+			return
+		}
+		users = append(users, user)
+	}
+	rows.Close()
+	return
+}
+
+// Get a single user given the email
+func UserByEmail(email string) (user User, err error) {
+	user = User{}
+	err = Db.QueryRow("SELECT id, uuid, name, email, password, created_at FROM users WHERE email = $1", email).
+		Scan(&user.Id, &user.Uuid, &user.Name, &user.Email, &user.Password, &user.CreatedAt)
+	return
+}
+
+// Get a single user given the UUID
+func UserByUUID(uuid string) (user User, err error) {
+	user = User{}
+	err = Db.QueryRow("SELECT id, uuid, name, email, password, created_at FROM users WHERE uuid = $1", uuid).
+		Scan(&user.Id, &user.Uuid, &user.Name, &user.Email, &user.Password, &user.CreatedAt)
+	return
+}
